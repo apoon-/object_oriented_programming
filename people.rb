aedccf4a06e0c8ce5ae31ebf1eced8b125ca42bb
apoon- <anson.os.poon@gmail.com>
2013-10-22 17:44:24 -0400

    corrected classes Question 1

--- people.rb
+++ people.rb
@@ -1,6 +1,19 @@
+#Question 5
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def greet 
+    puts "Hi, my name is #{@name}"
+  end
+end
+
+
 #Question 1, 2
 
-class Student 
+class Student < Person
   def learn
     puts "I get it"
   end
@@ -8,7 +21,7 @@ end
 
 #Question 3
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object"
   end
@@ -16,32 +29,22 @@ end
 
 #Question 4
 
-class Person
-  def initializer(name)
-    $name = name
-  end
-
-#Question 5
-
-  def greet 
-    puts "Hi, my name is #{$name}"
-  end
-end
 
 #Question 6
 
-instructor = Instructor.new
-instructor.name = Chris
+instructor = Instructor.new("Chris")
+puts "Instructor Greeting"
 puts instructor.greet
 
 
 #Question 7
 
-Student = Student.new
-student.name = Cristina
+student = Student.new("Cristina")
+puts "Student Greeting"
 puts student.greet
 
 #Question 8
+
 
 puts instructor.teach
 puts student.learn
